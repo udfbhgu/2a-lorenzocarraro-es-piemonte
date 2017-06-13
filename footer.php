86fc6ba6217eda1599e49ab840c6268c66838e0c
lorenzocarraro <lorenzocarraro80@gmail.com>
2017-06-13 15:09:37 +0200

    modificati file con codice php

--- footer.php
+++ footer.php
@@ -4,9 +4,6 @@
         <p>&copy; 2016 Company, Inc.</p>
       </footer>
 
-    </div><!--/.container-->
-
-
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/vendor/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
